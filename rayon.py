62b155907d6f72a1c8d0d67bfeee19f8d18263ff
LoganSiebert <logan.siebert.13@gmail.com>
2018-05-10 18:33:10 +0200

    Update rayon.py

--- rayon.py
+++ rayon.py
@@ -105,3 +105,14 @@ class Rayon(object):
                     ptintersects.append(p)
     
     return(ptintersects)
+
+def rayon_direct(start_point,end_point, murs):
+    #renvoie le rayon direct (dans une liste)
+    list_rayon = []
+    nouveau_rayon = Rayon(start_point)                        
+    nouveau_rayon.add_point_principal(end_point)
+    nouveau_rayon.find_all_intersections(murs) #determination des points de transmission
+    list_rayon.append(nouveau_rayon)
+    return list_rayon
+    return(ptintersects)
+    return(ptintersects)
